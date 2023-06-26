fd1c8fb0e466844256fca570e770f42b07557daa
Tim McNamara <code@timmcnamara.co.nz>
2023-06-26 22:29:34 +1200

    feat: Parse bulk strings

---
+++ rcom-9-parse-bulk-string/src/main.rs
@@ -0,0 +1,253 @@
+use std::env;
+use std::fmt::Display;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::str;
+
+static USAGE: &'static str = "
+rcom: command-line communicator for Redis-compatible databases
+
+Usage:
+  rcom [options] ping
+  rcom [options] get [<key>]
+  rcom [options] set <key> [<value>]
+  rcom (-h|--help)
+
+Options:
+  --server   The server to connect to [default: localhost:6379]
+  -x         Read the last argument from STDIN.
+";
+
+#[derive(Debug)]
+struct Config {
+    server: String,
+    command: Command,
+    _read_payload_from_stdin: bool,
+}
+
+#[derive(Debug)]
+enum Command {
+    Ping,
+    Get { key: Vec<u8> },
+    Set { key: Vec<u8>, value: Vec<u8> },
+}
+
+impl Command {
+    fn send<W: Write>(&self, dest: &mut W) -> std::io::Result<usize> {
+        let written = match self {
+            Command::Ping => dest.write(b"*1\r\n$4\r\nPING\r\n")?,
+            Command::Get { key } => encode_and_write(dest, b"GET", &[key])?,
+            Command::Set { key, value } => encode_and_write(dest, b"SET", &[key, value])?,
+        };
+
+        Ok(written)
+    }
+}
+
+fn encode_and_write<W: Write>(
+    dest: &mut W,
+    command: &[u8],
+    args: &[&[u8]],
+) -> std::io::Result<usize> {
+    let mut bytes_written = 0;
+    let n_args = (1 + args.len()).to_string();
+
+    bytes_written += dest.write(b"*")?;
+    bytes_written += dest.write(n_args.as_bytes())?;
+    bytes_written += dest.write(b"\r\n")?;
+
+    bytes_written += dest.write(b"$")?;
+    bytes_written += dest.write(command.len().to_string().as_bytes())?;
+    bytes_written += dest.write(b"\r\n")?;
+    bytes_written += dest.write(command)?;
+    bytes_written += dest.write(b"\r\n")?;
+
+    for arg in args {
+        bytes_written += dest.write(b"$")?;
+        bytes_written += dest.write(arg.len().to_string().as_bytes())?;
+        bytes_written += dest.write(b"\r\n")?;
+        bytes_written += dest.write(arg)?;
+        bytes_written += dest.write(b"\r\n")?;
+    }
+
+    Ok(bytes_written)
+}
+
+impl Config {
+    fn from_env() -> Result<Config, &'static str> {
+        let args: Vec<_> = env::args().into_iter().collect();
+        let mut pos = 1;
+        let mut server = "localhost:6379".to_string();
+        let mut use_stdin = false;
+
+        if args.len() == 1 {
+            usage();
+        }
+
+        for (i, pair) in args.as_slice().windows(2).enumerate() {
+            let mut pair = pair.iter();
+            match pair.next() {
+                Some(s) if s == "--server" => {
+                    server = pair.next().unwrap().clone();
+                    pos += i * 2;
+                }
+                Some(s) if s.starts_with("--server=") => {
+                    server = s["--server=".len()..].to_string();
+                    pos += i * 2 - 1;
+                }
+                Some(s) if s == "-h" || s == "--help" => {
+                    usage();
+                }
+                Some(s) if s == "-x" => {
+                    use_stdin = true;
+                    pos += 1;
+                }
+                Some(_) => continue,
+                None => todo!(),
+            }
+        }
+
+        let mut buffer = Vec::new();
+        if use_stdin {
+            let mut stdin = std::io::stdin();
+            stdin.read_to_end(&mut buffer).unwrap();
+        }
+
+        let command_raw = args[pos].to_uppercase();
+        let command = match (command_raw.as_str(), use_stdin) {
+            ("PING", _) => Command::Ping,
+            ("GET", false) => Command::Get {
+                key: args[pos + 1].clone().into(),
+            },
+            ("GET", true) => Command::Get { key: buffer },
+            ("SET", false) => Command::Set {
+                key: args[pos + 1].clone().into(),
+                value: args[pos + 2].clone().into(),
+            },
+            ("SET", true) => Command::Set {
+                key: args[pos + 1].clone().into(),
+                value: buffer,
+            },
+            _ => todo!("{command_raw}"),
+        };
+
+        Ok(Config {
+            server,
+            command,
+            _read_payload_from_stdin: use_stdin,
+        })
+    }
+}
+
+#[derive(Debug)]
+enum RedisMessage {
+    // Array(Vec<RedisMessage>),
+    BulkString(Vec<u8>),
+    Error(String),
+    // Integer(i64),
+    SimpleString(Vec<u8>),
+}
+
+impl RedisMessage {
+    fn from_bytes(input: &[u8]) -> std::io::Result<RedisMessage> {
+        let mut bytes = input.iter().peekable();
+        let mut payload = Vec::with_capacity(128);
+
+        let msg = match bytes.next() {
+            Some(b'+') => {
+                while let Some(byte) = bytes.next() {
+                    match (*byte, bytes.peek()) {
+                        (b'\r', Some(b'\n')) => break,
+                        _ => payload.push(*byte),
+                    }
+                }
+
+                RedisMessage::SimpleString(payload)
+            }
+            Some(b'-') => {
+                while let Some(byte) = bytes.next() {
+                    match (*byte, bytes.peek()) {
+                        (b'\r', Some(b'\n')) => break,
+                        _ => payload.push(*byte),
+                    }
+                }
+
+                let payload = String::from_utf8_lossy(&payload);
+                RedisMessage::Error(payload.to_string())
+            }
+            Some(b'$') => {
+                let mut header = Vec::with_capacity(16);
+                loop {
+                    match (bytes.next(), bytes.peek()) {
+                        (None, _) => break,
+                        (Some(b'\r'), Some(b'\n')) => {
+                            bytes.next();
+                            break;
+                        },
+                        (Some(&byte), _) => header.push(byte),
+                    }
+                }
+                let header = String::from_utf8_lossy(&header);
+                let bytes_to_read = header.parse::<usize>().map_err(|_err| {
+                    std::io::Error::new(
+                        std::io::ErrorKind::InvalidData,
+                        format!("{header} is not a number"),
+                    )
+                })?;
+
+                for _ in 0..bytes_to_read {
+                    if let Some(byte) = bytes.next() {
+                        payload.push(*byte);
+                    };
+                }
+
+                RedisMessage::BulkString(payload)
+            }
+            Some(_) => todo!(),
+            None => todo!(),
+        };
+
+        Ok(msg)
+    }
+}
+
+impl Display for RedisMessage {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RedisMessage::SimpleString(payload) | RedisMessage::BulkString(payload) => {
+                match std::str::from_utf8(&payload) {
+                    Ok(response) => writeln!(f, "{}", &response),
+                    Err(_invalid_utf8) => std::io::stdout()
+                        .write_all(&payload)
+                        .map_err(|_err| std::fmt::Error),
+                }?;
+            }
+            RedisMessage::Error(payload) => eprintln!("{payload}"),
+        }
+
+        Ok(())
+    }
+}
+
+fn usage() {
+    println!("{}", USAGE);
+    std::process::exit(1);
+}
+
+fn main() -> std::io::Result<()> {
+    let conf = Config::from_env().unwrap();
+    dbg!(&conf);
+
+    let mut stream = TcpStream::connect(conf.server)?;
+    let bytes_sent = conf.command.send(&mut stream)?;
+    eprintln!("info: {bytes_sent} bytes sent");
+
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer[..])?;
+
+    let msg = RedisMessage::from_bytes(&buffer)?;
+
+    println!("{msg}");
+
+    Ok(())
+}
